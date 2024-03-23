8d165aff32a22a208215c539169272395ccc60ea
Aoife Walshe <151844043+awalshe02@users.noreply.github.com>
2024-03-23 20:42:06 +0000

    Updated server.py
    layout/display a bit funky but works. type 'list' to list connections, type 'select 0' to privately connect to first client (1 for 2nd etc). type bye to end private connection

--- server.py
+++ server.py
@@ -1,6 +1,16 @@
 import socket
 import sys #implement command lines and terminal commmands
+import threading
+import time
+from queue import Queue
 
+NUM_THREAD = 2
+NUM_OF_JOBS = [1, 2] #job one is to listen and accept and job 2 is to send messages and handle connections with existing clients
+queue = Queue()
+total_connections = []
+total_addresses = []
+
+#thread 1 here that can listen and accept connections
 #making socket here
 def make_socket():
     try:
@@ -8,8 +18,8 @@ def make_socket():
         global port
         global socket
     
-        host = ''
-        port = 1200
+        host = ""
+        port = 1115
         socket = socket.socket() #socket made here
 
     except socket.error as message:
@@ -30,29 +40,121 @@ def binding_socket():
         print("Error with binding socket " +str(message) + '\n' + "Retry: ") #if there is an error message will print adn will try and bind again
         binding_socket()
 
-#accepting connection(s)
-def accepting_connection():
-    connection, address = socket.accept() #ip address and port stored in address  (a list) variable
-    print("Connection found: " + "ip address is = " + str(address[0] + " port num is = " + str(address[1])))
-    send_message(connection)
-    connection.close() #closing connection
+#saving clients to list that have connected
+def get_connections():
+    for i in total_connections:
+        i.close() #going through each connection and closing it
 
+    del total_connections[:] #deleting all connection in the list
+    del total_addresses[:] #deleting all addresses in the list
+
+    while True:
+        try:
+            connection, address = socket.accept()
+            socket.setblocking(1) #prevents time out (1 for true)
+
+            total_connections.append(connection) #adding connection to the list
+            total_addresses.append(address) #adding address to the list
+            print("Connection! : " + address[0])
+
+        except:
+            print("Error with connecting")
+
+#thread 2 here that can see, select and send messsages to clients
+def start_terminal():
+    while True:
+        command = input('terminal> ') #come up as directory on terminal
+        if 'list' in command: #gonna list out all clients that are connected
+            connection_list()
+
+        elif 'select' in command: #in order to chose a specific client 
+            connection = get_client(command)
+            if connection is not None: #if not NULL
+                send_message(connection)
+        
+        else:
+            print("don't know this command :/")
+
+
+#prints out clients we are connected to
+def connection_list():
+    list = ''
+
+    for i, connection in enumerate(total_connections): #i statrs at 0 and enumerate will increase by 1 each time (goes through total_connections list)
+        try:
+            connection.send(str.encode(' ')) #sending as test to see if we get a response (continues after except is yes)
+            connection.recv(20000) #big chunk size 
+        
+        except:
+            del total_connections[i]
+            del total_addresses[i]
+            continue #ignores the line below
+
+        #puts first element of the address list into the variable 'list'
+        list = str(i) + "   " + str(total_addresses[i][0]) + "   " + str(total_addresses[i][1]) + "\n"
+
+    print("clients connected list: " + "\n" + list)
+
+#choosing specific client and returns the value of the client (aka its number)
+def get_client(command):
+    try:
+        client_num = command.replace('select ', '') #replaceing select that is in commadn with a blank
+        client_num = int(client_num) #converting it into a number
+        connection = total_connections[client_num] #get object of connections
+        
+        print("connected to this client: " + str(total_addresses[client_num][0]))
+        print(str(total_addresses[client_num][0]) + ">", end="") #formating to look like new terminal just for specific client (123.456.789.0> )
+
+        return connection
+
+    except:
+        print("connection doesn't exist")
+        return None
+    
 #sending messages to client
 def send_message(connection):
     while True:
-        command = input()
-        if command == 'bye':
-            connection.close() #connection is closed
-            socket.close() #socket is close
-            sys.exit() #command prompt is closed
-        if len(str.encode(command)) > 0: #give length of string
-            connection.send(str.encode(command))
-            response = str(connection.recv(1024), "utf-8") #converting response from bytes to string 
-            print(response, end='') #goes onto new line after printing the response
+        try:
+            command = input()
+            if command == 'bye':
+                break #goes out of while loop (back to start_terminal)
+            if len(str.encode(command)) > 0: #give length of string that is converted to bytes
+                connection.send(str.encode(command))
+                response = str(connection.recv(20000), "utf-8") #converting response from bytes to string 
+                print(response, end='') #goes onto new line after printing the response
 
-def main():
-    make_socket()
-    binding_socket()
-    accepting_connection()
+        except:
+            print("error with sending message :/ ")
+            break #goes out of while loop (back to start_terminal)
 
-main()
+#making thread
+def make_thread():
+    for _ in range(NUM_THREAD): #for loop in the range of num of threads (aka 2)
+        thread = threading.Thread(target=action) #creating thread
+        thread.daemon = True #thread will end when program ends
+        thread.start() #starting thread
+
+#does next action in the queue
+def action():
+    while True:
+        x = queue.get()
+
+        if x == 1: #thread 1
+            make_socket()
+            binding_socket()
+            get_connections()
+
+        if x == 2: #thread 2
+            start_terminal()
+        
+        queue.task_done()
+
+def make_action():
+    for x in NUM_OF_JOBS:
+        queue.put(x) #putting number of jobs list into queue formate cause threads work woth queues
+
+    queue.join()
+
+
+make_thread()
+make_action()
