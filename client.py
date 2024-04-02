4a7e0796ee5b0291f7d72c5b90235a54717f0e80
AdityaChaudhry23 <128807624+AdityaChaudhry23@users.noreply.github.com>
2024-04-02 22:25:08 +0100

    Add files via upload

--- client.py
+++ client.py
@@ -1,25 +1,17 @@
 import socket
 
-# Function to connect to the server (peer) and send a message
-def connect_to_server(server_address):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Ipv4 and tcp connection
-    client_socket.connect(server_address)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = "127.0.0.1" #ip address of server
+port = 1113
 
-    while True:
-        print("Send message or type 'bye' to end connection: ")
-        message = input()
+client.connect((host, port)) #to establish connection
 
-        if message.lower() == 'bye':
-            break 
-
-        client_socket.send(message.encode())
-        response = client_socket.recv(1024)
-        print(f"Response from server: {response.decode()}")
-    client_socket.close()
-
-
-# Example usage
-if __name__ == "__main__":
-    server_address = ('172.27.107.120', 1200)  # Change depending on address
-    #message = "Hi from this client!"
-    connect_to_server(server_address)
+while True:
+    data = client.recv(1024)
+    print('Server>  ', data.decode(), '\n')
+    if 'any key to exit programm' in data.decode(): #if that in any part of recieved message
+        input('Reply>  ')
+        client.close()
+    else:
+        reply = input('Reply>  ')
+        client.send(reply.encode())
--- server.py
+++ server.py
@@ -1,47 +1,107 @@
 import socket
 import threading
+import time
 
-# Function to handle incoming connections
-def connection(client_socket, address):
-    print(f"Accepted connection from {address}")
+# Initialising global variables
+mutex = threading.Lock()
+all_connections = []
+all_votes = 0
+yes_votes = 0
+
+class handle_client(threading.Thread):
+    def __init__(self, client, address, connections):
+        super().__init__()
+        self.client = client
+        self.address = address
+        self.connections = connections
+        self.lives = 3
+
+    def run(self):
+        self.begin()
+
+    def begin(self):
+        try:
+            welcome_msg = 'You must solve this riddle in 3 attempts! "The more of this there is, the less you see. What is it?" \n'
+            self.client.send(welcome_msg.encode())
+            self.riddle('darkness')
+        finally:
+            self.client.close()
+
+    def riddle(self, answer):
+        try:
+            while self.lives > 0:
+                response = self.client.recv(1024).decode().strip()
+                if response.lower() == answer:
+                    self.success()
+                    return
+                else:
+                    self.lives -= 1
+                    if self.lives == 0:
+                        self.fail()
+                    else:
+                        attempts_msg = f'{self.lives} attempts remaining\n'
+                        self.client.send(attempts_msg.encode())
+        except Exception as e:
+            print("Error with riddle interaction:", e)
+
+    def success(self):
+        global all_votes, yes_votes
+        try:
+            completed_msg = 'Congrats, you solved the riddle! Do you want some information? (yes/no)\n'
+            self.client.send(completed_msg.encode())
+            response = self.client.recv(1024).decode().strip().lower()
+            with mutex:
+                all_votes += 1
+                if response == 'yes':
+                    yes_votes += 1
+            self.voting()
+        except Exception as e:
+            print("Error in success:", e)
+
+    def fail(self):
+        fail_msg = 'Failed security test. Press any key to exit program\n'
+        self.client.send(fail_msg.encode())
+        print("Client failed riddle test")
+        with mutex:
+            self.connections.remove(self.address)
+        self.client.close()
+
+    def voting(self):
+        global all_votes, yes_votes
+        try:
+            while all_votes < len(all_connections):
+                time.sleep(1)  # Wait for all votes
+            if (yes_votes / len(all_connections)) >= 0.5:
+                msg = 'Majority agrees.\n'
+            else:
+                msg = 'Program is closing, press any key to exit program\n'
+            self.client.send(msg.encode())
+        except Exception as e:
+            print("Error in voting:", e)
+
+    def end_conn(self):
+        msg = 'Program is closing, press any key to exit program\n'
+        self.client.send(msg.encode())
+        with mutex:
+            self.connections.remove(self.address)
+        self.client.close()
+
+def main():
+    host = "127.0.0.1"
+    port = 1113  # Ensure this matches the client configuration
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen()
+    print("Server listening")
+
     while True:
-        data = client_socket.recv(1024)
-        if not data:
-            break
-        if data.lower().strip() == 'bye':
-            break   
-        caps = data.upper()
-        client_socket.send(caps)
-
-    client_socket.close()
-    print(f"Connection closed with {address}")
-
-# Function to start listening for incoming connections
-def start_server():
-    host = '172.27.107.120'
-    port = 1200
-
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-    server_socket.listen(5)
-
-    print(f"Server listening on {host} : {port}")
-
-    while True:
-        client_socket, address = server_socket.accept()
-        client_handler = threading.Thread(target=connection, args=(client_socket, address))
-        client_handler.start()
-
-# Function to connect to other peers
-def connect_to_peer(peer_address, message):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(peer_address)
-    client_socket.send(message.encode())
-    response = client_socket.recv(1024)
-    print(f"Received response from {peer_address}: {response.decode()}")
-    client_socket.close()
+        client, address = server.accept()
+        print("Connection from:", address)
+        with mutex:
+            all_connections.append(address)
+        c_handler = handle_client(client, address, all_connections)
+        c_handler.start()
 
 if __name__ == "__main__":
-    # Start server in a separate thread
-    server_thread = threading.Thread(target=start_server)
-    server_thread.start()
+    main()
