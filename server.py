ff01a59ad754a5910a32833b9bd1fe666e2ddac5
Aoife Walshe <151844043+awalshe02@users.noreply.github.com>
2024-04-02 21:39:42 +0100

    Update server.py
    voting works when everyone says yes lol

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import time
 
 class handle_client(threading.Thread): #when called thread is made each time (aka thread made for each client)
     def __init__(self, client, address, connections):
@@ -14,14 +15,16 @@ class handle_client(threading.Thread): #when called thread is made each time (ak
 
     def begin(self): #sends riddle to each client when they connect
         try:
-            welcome_msg = 'you must solve this riddle in 3 attempts! "What question can you never answer yes to?" \n'
+            welcome_msg = 'you must solve this riddle in 3 attempts! "The more of this there is, the less you see. What is it?" \n'
             self.client.send(str.encode(welcome_msg))
             print("Riddle sent\n")
-            answer = 'are you asleep yet?'
+            answer = 'darkness'
             self.riddle(answer) #passing answer parameter
         
         finally: #ensures connection will close when riddle function done (may need to remove this depending on service)
             self.client.close()
+
+                
 
 
     def riddle(self, answer):
@@ -44,11 +47,16 @@ class handle_client(threading.Thread): #when called thread is made each time (ak
 
     def success(self):
         try:
+            global all_votes, yes_votes
             completed = 'Congrats you solved the riddle! Do you want some information?\n' #or service, tailor message to suit here (message must go here tho)
             self.client.send(str.encode(completed))
             response = self.client.recv(1024)
+            with mutex:
+                all_votes = all_votes + 1
             if response.decode() == 'yes':
-                self.service() #got to service function
+                with mutex:
+                    yes_votes = yes_votes + 1
+                self.voting() #go to servive function
             else:
                 self.end_conn() #conection closes otherwise
         except:
@@ -65,22 +73,20 @@ class handle_client(threading.Thread): #when called thread is made each time (ak
         except:
             print("Error with fail message")
 
-    def service(self):
-        voting = 'Do you want to participate in our addition service? yes/no'
-        self.client.send(str.encode(voting))
-        response = self.client.recv(1024)
-        if response.decode() == 'yes':
-            print("service!!!")
-            num_votes = num_votes + 1
-            addition = 'Enter number to be added'
-            self.client.send(str.encode(addition))
-            received_num = self.client.recv(1024).decode('utf-8')
-            num = int(received_num)
-            sum_clients += num
-            if num_votes >= (len(self.connections) / 2):
-                sum_message = 'The final sum from all clients is:' + str(sum_clients)
-                self.client.send(str.encode(sum_message))
+    def voting(self):
+        try:
+            global all_votes, yes_votes, all_connections
+            while all_votes != len(all_connections):
+                time.sleep(1) #thread goes to sleep until all votes have been done
 
+            percent = yes_votes/len(all_connections)
+            if percent >= 0.5:
+                msg = 'test'
+                self.client.send(str.encode(msg))
+            else:
+                self.end_conn()
+        except:
+            print("Error with voting")
 
     def end_conn(self):
         try:
@@ -101,14 +107,15 @@ def main():
     global all_connections #initialise list
     all_connections = []
 
-    global num_votes
-    num_votes = 0
+    global all_votes
+    all_votes = 0
 
-    global sum_clients
-    sum_clients = 0
+    global yes_votes
+    yes_votes = 0
+
 
     host = ""
-    port = 9999
+    port = 9996
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #ipv4 and tcp connection
     server.bind((host, port))
@@ -121,7 +128,7 @@ def main():
         print("Connection from: ", str(address))
         with mutex: #critical section
             all_connections.append(address) #add address to the list
-        
+
         
         c_handler = handle_client(client, address, all_connections) #calling function
         c_handler.start() #start function
