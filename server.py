f4b5c0c7e9bfcd40a5da5cfc38011d6819c4c318
AdityaChaudhry23 <128807624+AdityaChaudhry23@users.noreply.github.com>
2024-04-03 02:18:42 +0100

    Add files via upload

--- server.py
+++ server.py
@@ -1,82 +1,84 @@
 import socket
 import threading
-import time
 
-# Initialising global variables
+# Global variables
+pizza_item_list = ["Mozzarella", "Cheddar", "Parmesan", "Pepperoni", "Bacon", "Chicken", "Mushrooms", "Onions", "Bell Peppers"]
 mutex = threading.Lock()
+condition = threading.Condition(mutex)
 all_connections = []
-base_options = ["Neapolitan Crust", "New York-Style Crust", "Chicago Deep Dish Crust"]
+votes = {item: [] for item in pizza_item_list}  # Tracks votes for each topping
 
-def collect_votes(option):
-    """Collects votes for a given option from all clients."""
-    global all_connections
-    yes_votes = 0
-    no_votes = 0
-
-    for client, _ in all_connections:
-        try:
-            client.send(f"Do you want {option}? (yes/no)\n".encode())
-            response = client.recv(1024).decode().strip().lower()
-            if response == "yes":
-                yes_votes += 1
-            else:
-                no_votes += 1
-        except Exception as e:
-            print(f"Error collecting vote: {e}")
-    
-    return yes_votes, no_votes
-
-class ClientThread(threading.Thread):
+class ClientHandler(threading.Thread):
     def __init__(self, client, address):
         threading.Thread.__init__(self)
         self.client = client
         self.address = address
-        self.lives = 3
 
     def run(self):
-        try:
-            self.client.send("Solve this riddle: The more of this there is, the less you see. What is it?\n".encode())
-            while self.lives > 0:
-                answer = self.client.recv(1024).decode().strip().lower()
-                if answer == "darkness":
-                    self.vote_on_options()
-                    break
-                else:
-                    self.lives -= 1
-                    self.client.send(f"Incorrect. {self.lives} attempts left.\n".encode())
-            else:
-                self.client.send("Failed to solve the riddle. Disconnecting.\n".encode())
-        finally:
-            self.client.close()
+        self.client.sendall(b"Welcome! Solve this riddle: The more of this there is, the less you see. What is it?\n")
+        if self.solve_riddle():
+            for topping in pizza_item_list:
+                self.vote_on_topping(topping)
+        self.client.sendall(b"Thank you for participating. Goodbye!\n")
+        self.client.close()
+        with condition:
+            all_connections.remove(self)
+            condition.notify_all()  # Notify other threads in case they are waiting for votes
 
-    def vote_on_options(self):
-        for option in base_options:
-            yes_votes, no_votes = collect_votes(option)
-            if yes_votes > no_votes:
-                self.client.send(f"Majority chose {option}.\n".encode())
-                break
-            elif yes_votes == no_votes:
-                self.client.send(f"Tie. Choosing {option} by default.\n".encode())
-                break
-            # If majority says 'no', continue to the next option
-        self.client.send("Voting ended. Disconnecting.\n".encode())
+    def solve_riddle(self):
+        attempts = 3
+        while attempts > 0:
+            answer = self.client.recv(1024).decode().strip().lower()
+            if answer == "darkness":
+                self.client.sendall(b"Correct! Let's build your pizza.\n")
+                return True
+            else:
+                attempts -= 1
+                self.client.sendall(f"Incorrect. {attempts} attempts left.\n".encode())
+        return False
+
+    def vote_on_topping(self, topping):
+        self.client.sendall(f"Do you want to add {topping} to your pizza? (yes/no)\n".encode())
+        vote = self.client.recv(1024).decode().strip().lower()
+        
+        with condition:
+            votes[topping].append(vote)
+            condition.notify_all()  # Notify other threads that a vote has been cast
+            # Wait until all clients have voted
+            while len(votes[topping]) < len(all_connections):
+                condition.wait()
+
+        yes_votes = votes[topping].count('yes')
+        if yes_votes > len(all_connections) / 2:
+            with condition:
+                self.client.sendall(f"Ingredient Added: {topping}\n".encode())
+        else:
+            with condition:
+                self.client.sendall(f"Ingredient Not Added: {topping}\n".encode())
+
+        with condition:
+            # Reset votes for this topping for the next round
+            if all(len(votes[t]) == len(all_connections) for t in pizza_item_list):
+                votes[topping] = []
+
+def accept_connections(server):
+    while True:
+        client, address = server.accept()
+        print(f"Connection from {address}")
+        client_handler = ClientHandler(client, address)
+        with condition:
+            all_connections.append(client_handler)
+            condition.notify_all()  # Notify other threads in case they are waiting for more connections
+        client_handler.start()
 
 def main():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(("", 1113))
-    server_socket.listen(5)
-    print("Server listening...")
-
-    try:
-        while True:
-            client, address = server_socket.accept()
-            print(f"Connection from {address} has been established.")
-            with mutex:
-                all_connections.append((client, address))
-            client_thread = ClientThread(client, address)
-            client_thread.start()
-    finally:
-        server_socket.close()
+    host = ''
+    port = 1113
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen(5)
+    print(f"Server listening on port {port}")
+    accept_connections(server)
 
 if __name__ == "__main__":
     main()
